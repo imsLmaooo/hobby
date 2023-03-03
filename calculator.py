6238bfabb2db3419f33a063f371341cc7211a652
imsLmaooo <prosto.fasterr3@gmail.com>
2023-03-04 00:42:30 +0200

    added calculator | run calculator.py

--- calculator.py
+++ calculator.py
@@ -41,28 +41,28 @@ try:
 	while attempts > 0: # Цикл для подсчета попыток
 		arithmetic_sign = input(print_arithmetic_sign)
 		if arithmetic_sign == '+':
-		    num = int(input("Введите первое число: "))
-		    num_2 = int(input("Введите второе число: "))
-		    print(f"{num} + {num_2} =", add(num, num_2))
-		    break
+			num = int(input("Введите первое число: "))
+			num_2 = int(input("Введите второе число: "))
+			print(f"{num} + {num_2} =", add(num, num_2))
+			break
 		elif arithmetic_sign == '-':
-		    num = int(input("Введите первое число: "))
-		    num_2 = int(input("Введите второе число: "))
-		    print(f"{num} - {num_2} =", subtract(num, num_2))
-		    break
+			num = int(input("Введите первое число: "))
+			num_2 = int(input("Введите второе число: "))
+			print(f"{num} - {num_2} =", subtract(num, num_2))
+			break
 		elif arithmetic_sign == '*':
-		    num = int(input("Введите первое число: "))
-		    num_2 = int(input("Введите второе число: "))
-		    print(f"{num} * {num_2} =", multiply(num, num_2))
-		    break
+			num = int(input("Введите первое число: "))
+			num_2 = int(input("Введите второе число: "))
+			print(f"{num} * {num_2} =", multiply(num, num_2))
+			break
 		elif arithmetic_sign == '/':
-		    num = int(input("Введите первое число: "))
-		    num_2 = int(input("Введите второе число: "))
-		    print(f"{num} / {num_2} =", divide(num, num_2))
-		    break
+			num = int(input("Введите первое число: "))
+			num_2 = int(input("Введите второе число: "))
+			print(f"{num} / {num_2} =", divide(num, num_2))
+			break
 		else:
-		    attempts-=1
-		    print(f"Некорректный ввод. Осталось {attempts} попыток.")
+			attempts-=1
+			print(f"Некорректный ввод. Осталось {attempts} попыток.")
 except ZeroDivisionError: # Деление на ноль
 	print(f"Нельзя делить на {num_2}")
 except ValueError: # Ошибка значений
